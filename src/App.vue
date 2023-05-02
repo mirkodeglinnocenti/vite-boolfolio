160656d8c86e60a686afd81e53f665e85e968ccd
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-05-02 16:38:30 +0200

    FEAT print results in console.log from api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import ProjectCard from './components/ProjectCard.vue';
+
+export default (await import('vue')).defineComponent({
+  components: {
+    ProjectCard,
+  }
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <ProjectCard />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p v-for="project in projects" :key="project.id">{{ project.title }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default (await import('vue')).defineComponent({
+  data() {
+    return {
+        projects: [],
+    }
+  },
+  methods:{
+    fetchProjects(){
+        axios.get('http://127.0.0.1:8000/api/projects')
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+  },
+  mounted(){
+    this.fetchProjects()
+  }
+})
+</script>
+
+<style>
+</style>
