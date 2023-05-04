d72a17d8542fcc83981871b2baf2b8a6a53fec24
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-05-04 23:57:11 +0200

    FEAT print type and technologies added

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -3,8 +3,18 @@
     <div>
         <Default>
         <div class="about">
-            <div class="container">
-                <h1>Progetto</h1>
+
+            <div v-if="project" class="container">
+                <h1>{{ project.title }}</h1>
+                <p>
+                    {{ project.description }}
+                </p>
+                <p class="card-text"><strong>tipologia:</strong> {{ project.type ? project.type.type : " - " }}</p>
+                <p><strong>Tecnologie:</strong></p>
+                <ul class="d-flex gap-2" v-if="project.technologies && project.technologies.length > 0">
+                    <li class="badge rounded-pill text-bg-warning" v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</li>
+                </ul>
+                <p v-else>-</p>
             </div>
 
         </div>
@@ -28,19 +38,22 @@
             }
         },
         methods: {
-            fetchProject(){
+            fetchProject(slug){
                 axios.get(`http://127.0.0.1:8000/api/projects/${ slug }`)
                 .then(res => {
+                    console.log("data", res.data)
                     const { results } = res.data;
                     this.project = results;
+                    console.log("project", this.project)
                 })
                 .catch(err => {
                     console.log(err);
                 })
             }
         },
-        mounted() {
-            this.fetchProject();
+        created() {
+            this.fetchProject("nemo-fugit-odit");
+            console.log(this.slug)
         }
     })
 </script>
